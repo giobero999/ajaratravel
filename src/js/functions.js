f30241af562c1b7e994d0fea9292f06eeeccb575
Gio Beridze <giobero999@gmail.com>
2016-04-05 20:31:27 +0400

    home page

--- src/js/functions.js
+++ src/js/functions.js
@@ -24,4 +24,79 @@ $(document).ready(function(){
     }
   });
 
+  $('.carousel').carousel({
+		interval: 3000
+	});
+
+
+  var mql = window.matchMedia("screen and (max-width: 800px)");
+  if (mql.matches){ // if media query matches
+   $('.wrapper, header, footer, .slider, .filter').addClass('container');
+  }
+  else{
+   $('.wrapper, header, footer, .slider .filter').removeClass('container');
+  }
+
+  $("#ion-default, #ion-left").ionRangeSlider({
+    type: "double",
+    grid: false,
+    min: 0,
+    max: 1000,
+    from: 200,
+    to: 800,
+    prefix: "$"
+  });
+
+  $("#ion-advanced").ionRangeSlider({
+    type: "double",
+    grid: false,
+    min: 0,
+    max: 1000,
+    from: 200,
+    to: 800,
+    prefix: "$"
+  });
+
+  $('.select').select2({
+    minimumResultsForSearch: -1
+  });
+
+  $(function() {
+    $( "#from" ).datepicker({
+      defaultDate: "+1w",
+      changeMonth: true,
+      numberOfMonths: 1,
+      onClose: function( selectedDate ) {
+        $( "#to" ).datepicker( "option", "minDate", selectedDate );
+      }
+    });
+    $( "#to" ).datepicker({
+      defaultDate: "+1w",
+      changeMonth: true,
+      numberOfMonths: 1,
+      onClose: function( selectedDate ) {
+        $( "#from" ).datepicker( "option", "maxDate", selectedDate );
+      }
+    });
+  });
+
+  $(function() {
+    $( "#left-from" ).datepicker({
+      defaultDate: "+1w",
+      changeMonth: true,
+      numberOfMonths: 1,
+      onClose: function( selectedDate ) {
+        $( "#to" ).datepicker( "option", "minDate", selectedDate );
+      }
+    });
+    $( "#left-to" ).datepicker({
+      defaultDate: "+1w",
+      changeMonth: true,
+      numberOfMonths: 1,
+      onClose: function( selectedDate ) {
+        $( "#from" ).datepicker( "option", "maxDate", selectedDate );
+      }
+    });
+  });
+
 }); //end ready

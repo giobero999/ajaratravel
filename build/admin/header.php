f367870e913b0815deea8edf96ed355b086406e0
Gio Beridze <giobero999@gmail.com>
2016-04-19 20:23:08 +0400

    sidebar.php, header.php

---
+++ build/admin/header.php
@@ -0,0 +1,49 @@
+<!-- header section start-->
+<div class="header-section">
+
+    <!--logo and logo icon start-->
+    <div class="logo dark-logo-bg hidden-xs hidden-sm">
+        <a href="index.html">
+            <img src="img/logo-icon.png" alt="">
+            <!--<i class="fa fa-maxcdn"></i>-->
+            <span class="brand-name">Ajaratravel</span>
+        </a>
+    </div>
+
+    <div class="icon-logo dark-logo-bg hidden-xs hidden-sm">
+        <a href="index.html">
+            <img src="img/logo-icon.png" alt="">
+            <!--<i class="fa fa-maxcdn"></i>-->
+        </a>
+    </div>
+    <!--logo and logo icon end-->
+
+    <!--toggle button start-->
+    <a class="toggle-btn"><i class="fa fa-outdent"></i></a>
+    <!--toggle button end-->
+    <!--mega menu end-->
+    <div class="notification-wrap">
+        <div class="right-notification">
+            <ul class="notification-menu">
+                <li>
+                    <form class="search-content" action="index.html" method="post">
+                        <input type="text" class="form-control" name="keyword" placeholder="Search...">
+                    </form>
+                </li>
+
+                <li>
+                    <a href="javascript:;" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+                        <img src="img/avatar-mini.jpg" alt="">John Doe
+                        <span class=" fa fa-angle-down"></span>
+                    </a>
+                    <ul class="dropdown-menu dropdown-usermenu purple pull-right">
+                        <li><a href="login.html"><i class="fa fa-sign-out pull-right"></i> Log Out</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+        <!--right notification end-->
+    </div>
+
+</div>
+<!-- header section end-->
